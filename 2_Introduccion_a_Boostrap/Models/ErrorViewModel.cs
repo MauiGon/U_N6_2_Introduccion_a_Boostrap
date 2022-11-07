b9e24519f6af71fb3b532c75ca89e24aa911924c
Mauricio Alvarez <mauricioalvarez.or@gmail.com>
2022-11-07 18:48:55 -0300

    Agregar archivos de proyecto.

---
+++ 2_Introduccion_a_Boostrap/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _2_Introduccion_a_Boostrap.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
